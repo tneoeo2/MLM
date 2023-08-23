1e5de3d78239da9a9730503302c3262e908ec34a
tneoeo2 <tneoeo2@gmail.com>
2023-08-24 02:12:54 +0900

    타이머 에러 수정

--- module/timer.py
+++ module/timer.py
@@ -25,12 +25,12 @@ def worker(url=default_url):
 
 class SetTimer:
     def __init__(self, end_time: str = None):
-        if end_time is None:
+        self.end_time = end_time
+        if self.end_time is None:
             self.end_time = datetime.datetime.now() + datetime.timedelta(seconds=5)
             self.end_time = self.end_time.strftime("%H:%M:%S:%f")[:-3]
             logging.info("목표시간 : {}".format(self.end_time))
         else:
-            self.end_time = end_time
             logging.info("목표시간 : {}".format(self.end_time))
         self.current_time = '00:00:00.000' #해당 서버 현재시간
 
@@ -44,7 +44,7 @@ class SetTimer:
             s = int(self.current_time[6:8])
             msec = int(self.current_time[9:])
             formatted_time = f"{h:02}:{m:02}:{s:02}:{msec:03}"
-            logging.info(formatted_time)
+            logging.info("formatted_time : %s", formatted_time)
     
             if m == int(self.end_time[3:5]):  # 목표시간 도달하면
                 if s - 1 == int(self.end_time[6:8]) and msec >= 700 :
