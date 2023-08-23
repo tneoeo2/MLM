23f843469691c14521c9794052090e4da5d4cd31
tneoeo2 <tneoeo2@gmail.com>
2023-08-24 02:10:32 +0900

    티켓팅 스레드 수정

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ with open('test.json', encoding='UTF8') as f:
 TEST_ID = test_data['TEST_USER']['ID']
 TEST_PW = test_data['TEST_USER']['PW']
 TEST_BIRTH = test_data['TEST_USER']['BIRTH']
+
+
 TEST_MC = test_data['TEST_MC']['오유']
 CAPTCHA_URL = test_data['CAPTCHA_URL']
 
@@ -38,40 +40,51 @@ def get_driver():
     return driver
 
 
-def get_ticket():
-    #!크롬드라이버 
-    chrome_driver = get_driver()
-    logging.info("---드라이버 가져오기---")
-    #!사이트 접속(인터파크)
-    logging.info("---티켓팅 모듈 실행---")
-    tm = TicketModule(chrome_driver)
-    tm.login_go(INTERPARK_LOGIN_URL, TEST_ID, TEST_PW)
-    #!공연선택
-    tm.link_go(TEST_MC)
-    #!날짜 선택 & 7회차 선택
-    tm.date_select(['1', '13'], '1')
-    # tm.date_select(['1', '3'], '1')
-    #!캡차 유무 선택 
-    tm.read_captcha(url = CAPTCHA_URL, opt = 1)
-    #!좌석 선택
-    seat_name = ["VIP","R"]
-    tm.seat_select(1, seat_name=seat_name)
-    #!할인권종 선택
-    tm.discount(0)
-    #!결제 선택 - 무통장(0) -> 카카오(1)
-    tm.payment(0, birth=TEST_BIRTH)
 
-test_time = '14:01:20'    #?타이머 테스트용 
+class TicketThread():
+    def __init__(self, time=None):
+        self.time = time
+        
+    def start_driver(self):
+        logging.info("---드라이버 가져오기---")
+        #!크롬드라이버 
+        self.driver = get_driver()
+        #!사이트 접속(인터파크)
+        logging.info("---티켓팅 모듈 실행---")
+        self.tm = TicketModule(self.driver)
+        self.tm.login_go(INTERPARK_LOGIN_URL, TEST_ID, TEST_PW)
+    
+    def get_ticket(self):
+        logging.info('---공연선택 시작---')
+        #!공연선택
+        self.tm.link_go(TEST_MC)
+        #!날짜 선택 & 7회차 선택
+        self.tm.date_select(['0', '29'], '1')
+        # self.tm.date_select(['2', '3'], '1')
+        #!캡차 유무 선택 
+        self.tm.read_captcha(url = CAPTCHA_URL, opt = 1)
+        #!좌석 선택(좌석이름, 구역 순 입력)
+        seat_name = [["VIP", "B"], ["VIP", "C"], ["VIP", "A"], ["R", "B"], ["R", "A"], ["R", "C"]]
+        self.tm.seat_select(1, seat_name=seat_name)
+        #!할인권종 선택
+        self.tm.discount(0)
+        #!결제 선택 - 무통장(0) -> 카카오(1)
+        self.tm.payment(0, birth=TEST_BIRTH)
+
+    def run(self):
+        self.start_driver()
+        self.timer_instance = timer.SetTimer(self.time)
+        time_thread = threading.Thread(target=self.timer_instance.run)
+        logging.info("타임스레드 실행")
+        time_thread.start()
+        time_thread.join() #타이머 끝나면 티켓팅 시작
+        self.get_ticket()
+        
+# test_time = '14:00:00'    #?타이머 테스트용 
+test_time = '00:22:00'    #?타이머 테스트용 
 #!타임스레드 실행
 #?타이머 불러오기 (다른 스레드 생성하여 진행)
-timer_instance = timer.SetTimer()
-# timer_instance = timer.SetTimer(test_time)
-time_thread = threading.Thread(target=timer_instance.run)
-logging.info("타임스레드 실행")
-time_thread.start()
 
-time_thread.join()
-
-ticketing_thread = threading.Thread(target=get_ticket)
+ticketing_thread = threading.Thread(target=TicketThread().run)
 ticketing_thread.start()
 logging.info("티켓팅스레드 실행")
