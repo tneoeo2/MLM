d39904a5ea8a5b239ccc6296418bfb3c391c3336
tneoeo2 <tneoeo2@gmail.com>
2023-08-25 19:40:35 +0900

    main 인자 순서 수정

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ test_data  = ''
 with open('test.json', encoding='UTF8') as f:
     test_data = json.load(f)
     
-# TEST_ID = test_data['TEST_USER']['ID']
-# TEST_PW = test_data['TEST_USER']['PW']
-# TEST_BIRTH = test_data['TEST_USER']['BIRTH']
+TEST_ID = test_data['TEST_USER']['ID']
+TEST_PW = test_data['TEST_USER']['PW']
+TEST_BIRTH = test_data['TEST_USER']['BIRTH']
 
-TEST_ID = test_data['SOOIN']['ID']
-TEST_PW = test_data['SOOIN']['PW']
-TEST_BIRTH = test_data['SOOIN']['BIRTH']
+# TEST_ID = test_data['SOOIN']['ID']
+# TEST_PW = test_data['SOOIN']['PW']
+# TEST_BIRTH = test_data['SOOIN']['BIRTH']
 
 
 # TEST_MC = test_data['TEST_MC']['더데빌']
@@ -37,14 +37,6 @@ INTERPARK_LOGIN_URL = "https://ticket.interpark.com/Gate/TPLogin.asp"
 
 logging = CustomLogger("INFO").get_logger()
 
-#사이트 주소
-account = [TEST_ID, TEST_PW] #로그인 정보(ID, PW)
-t_date = [0, 22], #회차정보[[월, 일], 회차]
-t_time = 2
-is_cap = 0   #캡차유무 설정 (0: 미사용, 1: 사용)
-seat_cnt = 1 #좌석수
-payment = 0  #결제 방법 선택(무통장:0, 카카오:1)
-start_time = '14:00:00'    #?타이머 테스트용 
 
 
 #?웹드라이버 불러오기(크롬)
@@ -57,12 +49,13 @@ def get_driver():
 
 
 class TicketThread():
-    def __init__(self, time=None, account:list, t_date:list, t_time:int, is_cap:int, seat_cnt:int, payment:int):
+    def __init__(self, t_date:list, time=None, account:list=[TEST_ID, TEST_PW], t_time:int = 1, is_cap:int = 0, seat_cnt:int = 1, payment:int = 0):
         self.start_time = time
         self.account = account
         self.t_date = t_date
         self.t_time = t_time
         self.is_cap = is_cap
+        self.seat_cnt = seat_cnt
         self.payment = payment
         
     def start_driver(self):
@@ -80,7 +73,6 @@ class TicketThread():
         self.tm.common_link_go(TEST_MC)
         #!날짜 선택[현재월+n,n일, n회차] & 회차 선택
         self.tm.common_date_select(self.t_date, self.t_time)
-        # self.tm.common_date_select(['1', '22'], '1')
         #!캡차 유무 선택 
         self.tm.read_captcha(url = CAPTCHA_URL, opt = 0)
         #!좌석 선택(좌석수, [좌석이름, 구역] 입력)
@@ -103,10 +95,30 @@ class TicketThread():
         
 # test_time = '14:00:00'    #?타이머 테스트용 
 test_time = '13:00:00'    #?타이머 테스트용 
-# test_time = '00:22:00'    #?타이머 테스트용 
+# test_time = '00:22:00'    #?타이머 테스트용
+
+#! 시작 전, 필요 정보 입력 !
+account = [TEST_ID, TEST_PW] #로그인 정보(ID, PW)
+t_date = [10, 5] #회차정보[[월, 일]
+t_time = 1   # 회차
+is_cap = 0   #캡차유무 설정 (0: 미사용, 1: 사용)
+seat_cnt = 1 #좌석수
+payment = 0  #결제 방법 선택(무통장:0, 카카오:1)
+start_time = '14:00:00'    #?타이머 테스트용 
+
+
+ 
 #!타임스레드 실행
 #?타이머 불러오기 (다른 스레드 생성하여 진행)
-ticketing_thread = threading.Thread(target=TicketThread().run)
+ticketing_thread = threading.Thread(target=TicketThread(
+    # start_time=None, 
+    account=account, 
+    t_date=t_date,
+    t_time=t_time, 
+    is_cap=is_cap,
+    seat_cnt=seat_cnt,
+    payment=payment
+    ).run)
 # ticketing_thread = threading.Thread(target=TicketThread(test_time).run)
 ticketing_thread.start()
 logging.info("티켓팅스레드 실행")
